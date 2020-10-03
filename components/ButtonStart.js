fe649a4ea404029de0544ef84a90ec5d7805da12
Yasser <yassergeo@gmail.com>
2020-10-03 14:01:47 -0500

    added new styles to options button Crear listado de opciones  #3

--- components/ButtonStart.js
+++ components/ButtonStart.js
@@ -1,12 +1,21 @@
 import * as React from "react";
 import { Button } from "react-native";
+//import { StyleSheet } from "react-native";
 const ButtonStart = ({ navigation }) => {
   return (
     <Button
+      color="#215e97"
       title="Opciones"
       onPress={() => navigation.navigate("Options", {})}
     />
   );
 };
+
+/* const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#fff",
+    color: "red",
+  },
+}); */
 
 export default ButtonStart;
